3ae5907a65184e4bcfa1e42aaf0d0da99a95d49e
Israel-Ferreira <israelfsouza@hotmail.com>
2021-11-19 15:12:18 -0300

    Endpoint GET /users

--- src/main/java/io/codekaffee/quarkussocial/UserResource.java
+++ src/main/java/io/codekaffee/quarkussocial/UserResource.java
@@ -3,18 +3,33 @@ package io.codekaffee.quarkussocial;
 import io.codekaffee.quarkussocial.dto.CreateUserRequest;
 
 import javax.annotation.Resource;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 
 
-@Resource
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest createUserRequest){
         System.out.println(createUserRequest);
-        return Response.status(Response.Status.CREATED).build();
+        return Response.ok(createUserRequest).build();
+    }
+
+
+    @GET
+    public Response listAllUsers(){
+        List<CreateUserRequest> users = Arrays.asList(
+                new CreateUserRequest("Israel", 22),
+                new CreateUserRequest("Alexsander Vader", 22),
+                new CreateUserRequest("Ciro V.", 24)
+        );
+
+        return Response.ok(users).build();
     }
 }
